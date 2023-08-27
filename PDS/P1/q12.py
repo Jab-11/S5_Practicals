18248c073dd87250c490fbe7d83fb5860d1ae149
Jabarson <115206504+cjap1110@users.noreply.github.com>
2023-08-27 01:19:33 +0000

    pds-p1-q12-done

--- PDS/P1/q12.py
+++ PDS/P1/q12.py
@@ -2,6 +2,7 @@
 Write a python program to Sort the list according to the column using lambda. Display smallest
 and largest number from the list. Perform addition of all the members of list and display it. 
 """
+# Function to sort list according to the column
 def sort_col(list, c):
     list.sort(key=lambda x:x[c])
     print(f"Sorted list by col. {c}",list)
@@ -10,6 +11,7 @@ list=[["c",1972],["java", 1995], ["c++", 1983], ["python", 1989], ["kotlin",2011
 print("List : ",list)
 sort_col(list,1)
 
+# Creted a number list to find smallest, largest and sum.
 num_list = [34, 11, 35, 89, 37]
 print("\nNumber list : ",num_list)
 sort_col(num_list)
@@ -17,5 +19,3 @@ print("Smallest : ",min(num_list))
 print("Largest: ",max(num_list))
 print("Addition of all members : ",sum(num_list))
 
-# print(max(list[i][1] for i in range(len(list))))
-# print(min(list[i][1] for i in range(len(list))))
