8c6b64256711f8d3e28d545238bbacc5f53040a9
Jabarson <115206504+cjap1110@users.noreply.github.com>
2023-08-27 01:11:13 +0000

    pds-p1-q10-done

--- PDS/P1/q10.py
+++ PDS/P1/q10.py
@@ -2,15 +2,18 @@
 Write a program to count a total number of lines and count the total number of lines starting with
 ‘PDS’ from the given text file.
 """
+# Function to read a file
 def read_file(filename):
     with open(filename, "r") as f:
         content = f.read()
     return content
 
+# Function to count number of lines
 def count_lines(content):
     lines = content.count("\n")+1
     return lines
 
+# Function to count number of lines starting with PDS
 def count_pds(content):
     content=content.lower()
     pds_count = content.count("\npds")
