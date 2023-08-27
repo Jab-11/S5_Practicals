6a85b5360ab68038291830ec87a912c747bf9c55
Jabarson <115206504+cjap1110@users.noreply.github.com>
2023-08-27 01:08:27 +0000

    pds-p1-q9-done

--- PDS/P1/q9.py
+++ PDS/P1/q9.py
@@ -3,14 +3,18 @@ Write a program to find Highest Common Factor (HCF) and Greatest Common Divisor 
 two numbers using function.
 """
 def hcf(x,y):
+    # Create lists to store the divisors of x and y.
     div_x=[]
     div_y=[]
+     # Find divisors of both x and y.
     for i in range(1,min(x,y)+1):
         if x % i==0:
             div_x.append(i)
         if y % i==0:
             div_y.append(i)
+    # Find the common divisors of x and y by
     common = set(div_x).intersection(div_y)
+    # Return the largest value among the common divisors
     return max(common)
 
 n1 = int(input("Enter first number : "))
